8a953d64ab9eab89ef6b59321dd31fe2433353b9
Rahul Butani <rr.butani@gmail.com>
2018-09-11 00:25:08 -0500

    🎉

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
 extern crate ferris_says;
+extern crate motivations;
+extern crate rand;
 
 use ferris_says::say;
+use rand::Rng;
+use std::cmp;
 use std::io::{stdout, BufWriter};
 
 fn main() {
     let stdout = stdout();
-    let out = b"Hello fellow Rustaceans!";
-    let width = 24;
+    let out = rand::thread_rng()
+        .choose(&motivations::MOTIVATIONS)
+        .unwrap()
+        .as_bytes();
+    let width = cmp::min(40, out.len());
 
     let mut writer = BufWriter::new(stdout.lock());
     say(out, width, &mut writer).unwrap();
